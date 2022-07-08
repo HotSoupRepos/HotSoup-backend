2d724caaa2453e4dc12dee2bf3db5b2459e213ba
Ariel Steger <ariellekatherine92@gmail.com>
2022-07-08 16:25:06 -0400

    starting google api

---
+++ googleAPI.py
@@ -0,0 +1,10 @@
+import requests
+
+url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522%2C151.1957362&radius=1500&type=restaurant&keyword=cruise&key=YOUR_API_KEY"
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
