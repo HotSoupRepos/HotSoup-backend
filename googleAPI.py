43bd9ff299b6e49ee32e82998f5a38ad4c2b771a
Ariel Steger <ariellekatherine92@gmail.com>
2022-07-11 20:42:48 -0400

    added notes to googleAPI

--- googleAPI.py
+++ googleAPI.py
@@ -3,13 +3,16 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+#create your own .env file with your GOOGLE_MAP_API_KEY
 
 GOOGLE_MAP_API_KEY=os.getenv("GOOGLE_MAP_API_KEY")
+#use own API KEY from google cloud account - will consolidate to business api at later date
 
 def test_api_query (lat, long, type, keyword):
     
 
     url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{long}&radius=3000&type={type}&keyword={keyword}&key={GOOGLE_MAP_API_KEY}"
+    #default to 20 add parameter token to get up to 60 search results 
 
     payload={}
     headers = {}
@@ -19,4 +22,5 @@ def test_api_query (lat, long, type, keyword):
     print(response.text)
 
 test_api_query("41.18754","-73.2180112","food bank","food pantry")
+#test with long, lat, type, and keyword
 
