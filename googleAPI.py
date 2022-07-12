2b76041cb9e8a474d4ffd6050834a7d82ae2e4be
Ariel Steger <ariellekatherine92@gmail.com>
2022-07-11 20:28:01 -0400

    added .env

--- googleAPI.py
+++ googleAPI.py
@@ -1,9 +1,15 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GOOGLE_MAP_API_KEY=os.getenv("GOOGLE_MAP_API_KEY")
 
 def test_api_query (lat, long, type, keyword):
     
 
-    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{long}&radius=1500&type={type}&keyword={keyword}&key={YOUR-API-KEY-HERE}"
+    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{long}&radius=3000&type={type}&keyword={keyword}&key={GOOGLE_MAP_API_KEY}"
 
     payload={}
     headers = {}
