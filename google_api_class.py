824baddaf51de26c140c249902e1ef81f81dbbfb
sushrita <sushrita@umich.edu>
2022-07-25 14:55:33 -0400

    google_API_class.py changes

--- google_api_class.py
+++ google_api_class.py
@@ -2,14 +2,14 @@ import googleAPI
 import requests
 
 class Google_API:
-    def Maps_API_Request(self, api, parameters, lat, long, type, keyword):
+    def Maps_API_Request(self, parameters, lat, lng, type, keyword):
         parameters = {
             lat: "lat",
-            long: "long",
+            lng: "lng",
             type: "type",
             keyword: "keyword"
         }
-        url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{long}&radius=3000&type={type}&keyword={keyword}&key={GOOGLE_MAP_API_KEY}"
+        url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&radius=3000&type={type}&keyword={keyword}&key={GOOGLE_MAP_API_KEY}"
         response = requests.get(url, params=parameters)
         if response.status_code == 200:
             print("sucessfully fetched the data with parameters provided")
