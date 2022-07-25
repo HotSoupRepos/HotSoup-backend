fb0a78bc19ac628f1fafcfe2d81a75e766b91b9f
sushrita <sushrita@umich.edu>
2022-07-25 15:23:29 -0400

    changed function

---
+++ googleAPIclasses.py
@@ -0,0 +1,20 @@
+import googleAPI
+import requests
+
+class googleAPI:
+    def __createLink__(self, api, parameters, lat, long, type, keyword):
+        parameters = {
+            lat: "lat",
+            long: "long",
+            type: "type",
+            keyword: "keyword"
+            #ID?
+        }
+        url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{long}&radius=3000&type={type}&keyword={keyword}&key={GOOGLE_MAP_API_KEY}"
+        response = requests.get(url, params=parameters)
+        if response.status_code == 200:
+            print("sucessfully fetched the data with parameters provided")
+            self.formatted_print(response.json())
+        else:
+            print(
+                f"Hello person, there's a {response.status_code} error with your request")
